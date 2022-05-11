f095946fb8e61ca76607ed5b4e03cf2be22fde2a
AnshumanBiswal <abiswal@vmware.com>
2022-05-11 16:53:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+console.log('My First NPM package');
+
+console.log(chalk.green('I am a green line')+ chalk.blue.underline.bold('with a blue substring')+'that becomes green again' + chalk.red(' Red color text'));
+
